46cb7655560b4006a077f73b28eba07f72659aff
jagadish17 <115479432+jagadish17@users.noreply.github.com>
2022-10-14 11:25:47 +0530

    Add files via upload

---
+++ day4/MissingElement.java
@@ -0,0 +1,22 @@
+package weekk1.day4;
+
+import java.util.Arrays;
+
+public class MissingElement {
+	
+		public static void main(String[] args)
+		{
+		int[] Num= {1,2,3,4,5,7,8,10};
+		
+		int Sum=Num.length;
+		int misnum;
+		
+		Arrays.sort(Num);
+		for(int i=0;i<Sum;i++)
+		{
+			misnum=i+1;
+			if(misnum!=Num[i]) 
+			{
+				System.out.println(misnum);
+				break;
+}}}}
