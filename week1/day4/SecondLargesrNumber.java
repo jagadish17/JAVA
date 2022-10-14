5caa04da83dc84a73314d585e849ad14d77059a2
jagadish17 <115479432+jagadish17@users.noreply.github.com>
2022-10-14 11:33:38 +0530

    Add files via upload

---
+++ week1/day4/SecondLargesrNumber.java
@@ -0,0 +1,18 @@
+package week1.day4;
+
+import java.util.Arrays;
+
+public class SecondLargesrNumber {
+	public static void main(String[] args) {
+	int num[]= {10,2,4,6,87,11};
+	int data=num.length;
+	Arrays.sort(num);
+	for (int i = 0; i < num.length; i++) {
+		
+	}
+	System.out.println("Second largest number is "+num[data-2]);
+	
+	
+	}
+	
+}
