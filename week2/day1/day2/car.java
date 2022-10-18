feef293cdc6fa9228cb4bf40169454d6149d87bd
jagadish17 <115479432+jagadish17@users.noreply.github.com>
2022-10-18 11:37:45 +0530

    Add files via upload

---
+++ week2/day1/day2/car.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+public class car {
+	public static void main(String[] args) {
+		for (int i =1; i>20; i++) {
+			if(i%2==0) {
+				System.out.println(i +"is a even number");
+			}
+			else{
+				System.out.println(i +"is a odd number");
+			}
+
+				
+			}
+			
+	}}
+			
