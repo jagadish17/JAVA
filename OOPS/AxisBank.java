9c9df772dd2c0f86611ed5e730c00fc16832723c
jagadish17 <115479432+jagadish17@users.noreply.github.com>
2022-11-03 09:28:11 +0530

    Add files via upload

---
+++ OOPS/AxisBank.java
@@ -0,0 +1,14 @@
+package OOPS;
+
+public class AxisBank extends BankInfo {
+	public void deposit() {
+		System.out.println("saving =6000");
+	}
+public static void main(String[] args) {
+	AxisBank Bank=new AxisBank();
+	Bank.saving();
+	Bank.fixed();
+	Bank.deposit();
+	System.out.println(Bank);
+}
+}
---
+++ OOPS/BankInfo.java
@@ -0,0 +1,25 @@
+package OOPS;
+
+public class BankInfo {
+	public void saving() {
+		System.out.println("saving=12000");
+	
+	}
+	public void fixed() {
+		System.out.println("fixed=8000");
+		
+	}
+	public void deposit() {
+		System.out.println("deposit=10000");
+		
+	}
+	public static void main(String[] args) {
+		BankInfo info= new BankInfo();
+		info.saving();
+		info.fixed();
+		info.deposit();
+		System.out.println(info);
+		
+	}
+
+}
